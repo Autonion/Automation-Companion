f4327cf040b91ed76107a8713917348d4605750c
Guru Preetam Bodapati <gurupreetambodapati@gmail.com>
2026-05-06 18:05:10 +0530

    Updated version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.autonion.automationcompanion"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.5"
+        versionCode = 5
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
